bcb182cae766266fcd424b97b602d05bba63613e
ClaudiaBee <claudiabellan.herb@gmail.com>
2018-06-28 18:38:01 +0200

    Add logic to FloraOfNaviglioFragment and populate it with sample data

--- app/src/main/java/com/example/claudiabee/gorgotourguide/fragments/FloraOfNaviglioFragment.java
+++ app/src/main/java/com/example/claudiabee/gorgotourguide/fragments/FloraOfNaviglioFragment.java
@@ -1,25 +1,61 @@
 package com.example.claudiabee.gorgotourguide.fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.claudiabee.gorgotourguide.InfoCard;
+import com.example.claudiabee.gorgotourguide.InfoCardRecyclerAdapter;
 import com.example.claudiabee.gorgotourguide.R;
+
+import java.util.ArrayList;
 
 
 public class FloraOfNaviglioFragment extends Fragment {
+
+    private RecyclerView mCardRecyclerView;
+    private RecyclerView.LayoutManager mLinearLayoutLayout;
+    private RecyclerView.Adapter mAdapter;
 
         public FloraOfNaviglioFragment() {
         // Required empty public constructor
     }
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_flora_of_naviglio, container, false);
+        View rootView = inflater.inflate(R.layout.card_recycler, container, false);
+
+        // Create a list of InfoCard
+        ArrayList<InfoCard> infoCards = new ArrayList<InfoCard>();
+        infoCards.add(new InfoCard("Geranio", R.drawable.geranio));
+        infoCards.add(new InfoCard("Rubus", R.drawable.geranio));
+        infoCards.add(new InfoCard("Galium", R.drawable.geranio));
+        infoCards.add(new InfoCard("Ficus", R.drawable.geranio));
+        infoCards.add(new InfoCard("Sambucus", R.drawable.geranio));
+        infoCards.add(new InfoCard("Tilium", R.drawable.geranio));
+        infoCards.add(new InfoCard("Morus", R.drawable.geranio));
+
+        // Instantiate a RecyclerView object
+        mCardRecyclerView = rootView.findViewById(R.id.card_recycler);
+
+        // Instantiate a LinearLayoutManager to manage the RecyclerView
+        mLinearLayoutLayout = new LinearLayoutManager(getActivity());
+
+        // Set the LinearLayoutManager on the RecyclerView
+        mCardRecyclerView.setLayoutManager(mLinearLayoutLayout);
+
+        // Get an instance of InfoCardRecyclerAdapter
+        mAdapter = new InfoCardRecyclerAdapter(infoCards);
+
+        // Set the adapter on the RecyclerView
+        mCardRecyclerView.setAdapter(mAdapter);
+
+        return rootView;
     }
 }
