14a8c4abc5d20c72e71d44b2f4621cd973cbb283
ClaudiaBee <claudiabellan.herb@gmail.com>
2018-06-28 18:26:40 +0200

    Refactor GeneralInfoFragment renaming custom class object and ArrayList

--- app/src/main/java/com/example/claudiabee/gorgotourguide/fragments/GeneralInfoFragment.java
+++ app/src/main/java/com/example/claudiabee/gorgotourguide/fragments/GeneralInfoFragment.java
@@ -9,8 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.claudiabee.gorgotourguide.Info;
-import com.example.claudiabee.gorgotourguide.InfoRecyclerAdapter;
+import com.example.claudiabee.gorgotourguide.InfoCard;
+import com.example.claudiabee.gorgotourguide.InfoCardRecyclerAdapter;
 import com.example.claudiabee.gorgotourguide.R;
 
 import java.util.ArrayList;
@@ -33,22 +33,24 @@ public class GeneralInfoFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.card_recycler, container, false);
 
         // Create a list of Info
-        ArrayList<Info> infos = new ArrayList<Info>();
-        infos.add(new Info("Lorem Ipsum", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et mattis tellus, finibus egestas ipsum. Cras faucibus laoreet libero consequat imperdiet. Vivamus egestas a ligula eu tempus.", R.drawable.pontesullamartesana));
-        infos.add(new Info("Etiam justo odio", "Etiam justo odio, blandit non luctus sed, aliquam eu neque. ", R.drawable.segnaletica_ciclovia));
-        infos.add(new Info("Vestibulum euismod", "Vestibulum euismod ante lorem, ac bibendum quam aliquet vel. In ac consectetur massa. ", R.drawable.geranio));
-        infos.add(new Info("Morbi tincidunt", "Morbi tincidunt gravida mauris, at suscipit quam aliquam et. Donec euismod arcu est, vel mattis nisl scelerisque auctor.", R.drawable.pontesullamartesana));
-        infos.add(new Info("Phasellus gravida", "Phasellus gravida dignissim neque, sed dignissim est. Sed non felis in magna lobortis molestie. Proin at vehicula tellus.", R.drawable.pontesullamartesana));
+        ArrayList<InfoCard> infoCards = new ArrayList<InfoCard>();
+        infoCards.add(new InfoCard("Lorem Ipsum", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et mattis tellus, finibus egestas ipsum. Cras faucibus laoreet libero consequat imperdiet. Vivamus egestas a ligula eu tempus.", R.drawable.pontesullamartesana));
+        infoCards.add(new InfoCard("Etiam justo odio", "Etiam justo odio, blandit non luctus sed, aliquam eu neque. ", R.drawable.segnaletica_ciclovia));
+        infoCards.add(new InfoCard("Vestibulum euismod", "Vestibulum euismod ante lorem, ac bibendum quam aliquet vel. In ac consectetur massa. ", R.drawable.geranio));
+        infoCards.add(new InfoCard("Morbi tincidunt", "Morbi tincidunt gravida mauris, at suscipit quam aliquam et. Donec euismod arcu est, vel mattis nisl scelerisque auctor.", R.drawable.pontesullamartesana));
+        infoCards.add(new InfoCard("Phasellus gravida", "Phasellus gravida dignissim neque, sed dignissim est. Sed non felis in magna lobortis molestie. Proin at vehicula tellus.", R.drawable.pontesullamartesana));
 
         // Instantiate a RecyclerView object
         mCardRecyclerView = rootView.findViewById(R.id.card_recycler);
 
         //Instantiate a LinearlayoutManager to manage my cardRecyclerView
         mLinearLayoutManager = new LinearLayoutManager(getActivity());
+
+        // Set the LinearLayoutManager on the RecyclerView
         mCardRecyclerView.setLayoutManager(mLinearLayoutManager);
 
-        // Get an instance of InfoRecyclerAdapter
-        mAdapter = new InfoRecyclerAdapter(infos);
+        // Get an instance of InfoCardRecyclerAdapter
+        mAdapter = new InfoCardRecyclerAdapter(infoCards);
         // Set the adapter on the cardRecyclerView
         mCardRecyclerView.setAdapter(mAdapter);
 
