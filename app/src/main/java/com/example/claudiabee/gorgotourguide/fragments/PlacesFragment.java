44c7c6f401651d5f788756dddb7cb9dd96458a71
ClaudiaBee <claudiabellan.herb@gmail.com>
2018-06-28 18:29:10 +0200

    Add logic to PlacesFragment and populate it with sample data

--- app/src/main/java/com/example/claudiabee/gorgotourguide/fragments/PlacesFragment.java
+++ app/src/main/java/com/example/claudiabee/gorgotourguide/fragments/PlacesFragment.java
@@ -1,16 +1,24 @@
 package com.example.claudiabee.gorgotourguide.fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.claudiabee.gorgotourguide.InfoCard;
+import com.example.claudiabee.gorgotourguide.InfoCardRecyclerAdapter;
 import com.example.claudiabee.gorgotourguide.R;
 
+import java.util.ArrayList;
+
 public class PlacesFragment extends Fragment {
+
+    private RecyclerView mCardRecyclerView;
+    private RecyclerView.LayoutManager mLinearLayoutManager;
+    private RecyclerView.Adapter mAdapter;
 
     public PlacesFragment() {
         // Required empty public constructor
@@ -19,7 +27,32 @@ public class PlacesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_places, container, false);
+        View rootView = inflater.inflate(R.layout.card_recycler, container, false);
+
+        // Create a list of InfoCard
+        ArrayList<InfoCard> infoCards = new ArrayList<InfoCard>();
+        infoCards.add(new InfoCard("Sed ut perspiciatis", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."));
+        infoCards.add(new InfoCard("Nemo enim", "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."));
+        infoCards.add(new InfoCard("Neque porro", "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem."));
+        infoCards.add(new InfoCard("Ut enim", "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?"));
+        infoCards.add(new InfoCard("Quis autem", "Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"));
+
+        // Instantiate a RecyclerView object
+        mCardRecyclerView = rootView.findViewById(R.id.card_recycler);
+
+        // Instantiate a LinearLayoutManager to manage the RecyclerView
+        mLinearLayoutManager = new LinearLayoutManager(getActivity());
+
+        // Set the LinearLayoutManager on the RecyclerView
+        mCardRecyclerView.setLayoutManager(mLinearLayoutManager);
+
+        // Get an instance of InfoCardRecyclerAdapter
+        mAdapter = new InfoCardRecyclerAdapter(infoCards);
+
+        // Set the adapter on the RecyclerView
+        mCardRecyclerView.setAdapter(mAdapter);
+
+        return rootView;
     }
 
 }
